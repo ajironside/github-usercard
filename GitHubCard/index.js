cb35db07c39041e47bc790d6ec6ff73a1e8a1471
Amanda Ironside <ironside.amanda@gmail.com>
2020-02-17 15:22:29 -0700

    step one completed, data copied into step two comment, github request commented out

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,11 +1,58 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
+/* Step 1: 
+using axios, send a GET request to the following URL 
+(replacing the palceholder with your Github name):
+https://api.github.com/users/ajironside
 */
+
+/* axios.get('https://api.github.com/users/ajironside')
+    .then(function(response) {
+        // handle success
+        console.log(response)
+    })
+    .catch(function(error) {
+        // handle error
+        console.log(error)
+    })
+    .then(function() {
+        // always executed
+    }); */
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
+
+   data:
+    login: "ajironside"
+    id: 29545461
+    node_id: "MDQ6VXNlcjI5NTQ1NDYx"
+    avatar_url: "https://avatars0.githubusercontent.com/u/29545461?v=4"
+    gravatar_id: ""
+    url: "https://api.github.com/users/ajironside"
+    html_url: "https://github.com/ajironside"
+    followers_url: "https://api.github.com/users/ajironside/followers"
+    following_url: "https://api.github.com/users/ajironside/following{/other_user}"
+    gists_url: "https://api.github.com/users/ajironside/gists{/gist_id}"
+    starred_url: "https://api.github.com/users/ajironside/starred{/owner}{/repo}"
+    subscriptions_url: "https://api.github.com/users/ajironside/subscriptions"
+    organizations_url: "https://api.github.com/users/ajironside/orgs"
+    repos_url: "https://api.github.com/users/ajironside/repos"
+    events_url: "https://api.github.com/users/ajironside/events{/privacy}"
+    received_events_url: "https://api.github.com/users/ajironside/received_events"
+    type: "User"
+    site_admin: false
+    name: "Amanda Ironside"
+    company: null
+    blog: ""
+    location: "NM"
+    email: null
+    hireable: null
+    bio: null
+    public_repos: 105
+    public_gists: 0
+    followers: 1
+    following: 1
+    created_at: "2017-06-19T13:58:03Z"
+    updated_at: "2020-02-07T15:52:16Z"
 
    Skip to Step 3.
 */
