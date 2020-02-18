2470e15211991e7c5404f6410305935c9b943ba0
Amanda Ironside <ironside.amanda@gmail.com>
2020-02-18 09:33:50 -0700

    static data object created for testing

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,55 +4,63 @@ using axios, send a GET request to the following URL
 https://api.github.com/users/ajironside
 */
 
-/* axios.get('https://api.github.com/users/ajironside')
+/* const entryPointInHTML = document.querySelector('.cards')
+
+axios.get('https://api.github.com/users/ajironside')
     .then(response => {
         // handle success
         console.log(response)
+
+        response.data.message.forEach(url => {
+          const newUser = userCard(url);
+          entryPointInHTML.appendChild(newUser);
+        })
     })
     .catch(error => {
         // handle error
-        console.log(error)
+        console.log('Server could not find user data', error)
     }) */
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
-
-   data:
-    login: "ajironside"
-    id: 29545461
-    node_id: "MDQ6VXNlcjI5NTQ1NDYx"
-    avatar_url: "https://avatars0.githubusercontent.com/u/29545461?v=4"
-    gravatar_id: ""
-    url: "https://api.github.com/users/ajironside"
-    html_url: "https://github.com/ajironside"
-    followers_url: "https://api.github.com/users/ajironside/followers"
-    following_url: "https://api.github.com/users/ajironside/following{/other_user}"
-    gists_url: "https://api.github.com/users/ajironside/gists{/gist_id}"
-    starred_url: "https://api.github.com/users/ajironside/starred{/owner}{/repo}"
-    subscriptions_url: "https://api.github.com/users/ajironside/subscriptions"
-    organizations_url: "https://api.github.com/users/ajironside/orgs"
-    repos_url: "https://api.github.com/users/ajironside/repos"
-    events_url: "https://api.github.com/users/ajironside/events{/privacy}"
-    received_events_url: "https://api.github.com/users/ajironside/received_events"
-    type: "User"
-    site_admin: false
-    name: "Amanda Ironside"
-    company: null
-    blog: ""
-    location: "NM"
-    email: null
-    hireable: null
-    bio: null
-    public_repos: 105
-    public_gists: 0
-    followers: 1
-    following: 1
-    created_at: "2017-06-19T13:58:03Z"
-    updated_at: "2020-02-07T15:52:16Z"
-
+   
    Skip to Step 3.
 */
+
+const data = [{ //static example object
+    login: "ajironside",
+    id: 29545461,
+    node_id: "MDQ6VXNlcjI5NTQ1NDYx",
+    avatar_url: "https://avatars0.githubusercontent.com/u/29545461?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/ajironside",
+    html_url: "https://github.com/ajironside",
+    followers_url: "https://api.github.com/users/ajironside/followers",
+    following_url: "https://api.github.com/users/ajironside/following{/other_user}",
+    gists_url: "https://api.github.com/users/ajironside/gists{/gist_id}",
+    starred_url: "https://api.github.com/users/ajironside/starred{/owner}{/repo}",
+    subscriptions_url: "https://api.github.com/users/ajironside/subscriptions",
+    organizations_url: "https://api.github.com/users/ajironside/orgs",
+    repos_url: "https://api.github.com/users/ajironside/repos",
+    events_url: "https://api.github.com/users/ajironside/events{/privacy}",
+    received_events_url: "https://api.github.com/users/ajironside/received_events",
+    type: "User",
+    site_admin: false,
+    name: "Amanda Ironside",
+    company: null,
+    blog: "",
+    location: "NM",
+    email: null,
+    hireable: null,
+    bio: null,
+    public_repos: 105,
+    public_gists: 0,
+    followers: 1,
+    following: 1,
+    created_at: "2017-06-19T13:58:03Z",
+    updated_at: "2020-02-07T15:52:16Z"
+}]
 
 /* Step 4: 
 Pass the data received from Github into your function, 
@@ -71,7 +79,13 @@ Using that array, iterate over it, requesting data for each user,
 creating a new card for each user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+    'tetondan',
+    'dustinmyers',
+    'justsml',
+    'luishrd',
+    'bigknell'
+]
 
 /* Step 3: 
 Create a function that accepts a single object as its only argument,
