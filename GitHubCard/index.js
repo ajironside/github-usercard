3770b1c4d64c3f65d10b959cf8ff1bdf06d19817
Amanda Ironside <ironside.amanda@gmail.com>
2020-02-18 10:27:32 -0700

    working through errors in function

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,14 +4,19 @@ using axios, send a GET request to the following URL
 https://api.github.com/users/ajironside
 */
 
-/* const entryPointInHTML = document.querySelector('.cards')
+const entryPointInHTML = document.querySelector('.cards')
 
-axios.get('https://api.github.com/users/ajironside')
+const followersArray = [
+    'tetondan',
+    'dustinmyers',
+    'justsml',
+    'luishrd',
+    'bigknell'
+]
+
+/*axios.get('https://api.github.com/users/ajironside')
     .then(response => {
-        // handle success
-        console.log(response)
-
-        response.data.message.forEach(url => {
+          response.data.message.forEach(url => {
           const newUser = userCard(url);
           entryPointInHTML.appendChild(newUser);
         })
@@ -24,42 +29,19 @@ axios.get('https://api.github.com/users/ajironside')
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
-   
    Skip to Step 3.
 */
 
-const data = [{ //static example object
+//static example object
+const data = [{
     login: "ajironside",
-    id: 29545461,
-    node_id: "MDQ6VXNlcjI5NTQ1NDYx",
     avatar_url: "https://avatars0.githubusercontent.com/u/29545461?v=4",
-    gravatar_id: "",
-    url: "https://api.github.com/users/ajironside",
     html_url: "https://github.com/ajironside",
-    followers_url: "https://api.github.com/users/ajironside/followers",
-    following_url: "https://api.github.com/users/ajironside/following{/other_user}",
-    gists_url: "https://api.github.com/users/ajironside/gists{/gist_id}",
-    starred_url: "https://api.github.com/users/ajironside/starred{/owner}{/repo}",
-    subscriptions_url: "https://api.github.com/users/ajironside/subscriptions",
-    organizations_url: "https://api.github.com/users/ajironside/orgs",
-    repos_url: "https://api.github.com/users/ajironside/repos",
-    events_url: "https://api.github.com/users/ajironside/events{/privacy}",
-    received_events_url: "https://api.github.com/users/ajironside/received_events",
-    type: "User",
-    site_admin: false,
     name: "Amanda Ironside",
-    company: null,
-    blog: "",
     location: "NM",
-    email: null,
-    hireable: null,
     bio: null,
-    public_repos: 105,
-    public_gists: 0,
     followers: 1,
-    following: 1,
-    created_at: "2017-06-19T13:58:03Z",
-    updated_at: "2020-02-07T15:52:16Z"
+    following: 1
 }]
 
 /* Step 4: 
@@ -78,14 +60,6 @@ and add them as Individual strings to the friendsArray below.
 Using that array, iterate over it, requesting data for each user, 
 creating a new card for each user, and adding that card to the DOM.
 */
-
-const followersArray = [
-    'tetondan',
-    'dustinmyers',
-    'justsml',
-    'luishrd',
-    'bigknell'
-]
 
 /* Step 3: 
 Create a function that accepts a single object as its only argument,
@@ -110,38 +84,64 @@ create a component that will return the following DOM element:
 
 const createCard = (data) => {
     //create elements
-    const userCard = document.createElement('div');
-    const newImage = document.createElement('img');
-    const cardInfo = document.createElement('div');
-    const userName = document.createElement('h3');
-    const gitName = document.createElement('p');
-    const userLoc = document.createElement('p');
-    const userProfile = document.createElement('p');
-    const userPage = document.createElement('a');
-    const userFollowers = document.createElement('p');
-    const userFollowing = document.createElement('p');
-    const userBio = document.createElement('p');
-
     //append elements
+    //add classes
+    //add textContent
+    const userCard = document.createElement('div');
     userCard.appendChild(newImage);
     userCard.appendChild(cardInfo);
+    userCard.classList.add('card');
+
+    const newImage = document.createElement('img');
+    newImage.src = data.avatar_url;
+
+    const cardInfo = document.createElement('div');
     cardInfo.appendChild(userName);
     cardInfo.appendChild(gitName);
     cardInfo.appendChild(userLoc);
     cardInfo.appendChild(userProfile);
-    userProfile.appendChild(userPage);
     cardInfo.appendChild(userFollowers);
     cardInfo.appendChild(userFollowing);
     cardInfo.appendChild(userBio);
-
-    //add classes
-    userCard.classList.add('card');
     cardInfo.classList.add('card-info');
+
+    const userName = document.createElement('h3');
     userName.classList.add('name');
+    userName.textContent = data.name;
+
+    const gitName = document.createElement('p');
     gitName.classList.add('username');
+    gitName.textContent = data.login;
+
+    const userLoc = document.createElement('p');
+    userLoc.textContent = location;
+
+    const userProfile = document.createElement('p');
+    userProfile.appendChild(userPage);
+    userProfile.textContent = "Profile: ";
+
+    const userPage = document.createElement('a');
+    userPage.src = data.html_url;
+
+    const userFollowers = document.createElement('p');
+    userFollowers.textContent = data.followers;
+
+    const userFollowing = document.createElement('p');
+    userFollowing.textContent = data.following;
+
+    const userBio = document.createElement('p');
+    userBio.textContent = data.bio;
 
     return userCard;
 }
+
+//grab parent element
+const page = document.querySelector('.container');
+
+//append to parent element
+data.map(userData => {
+    page.appendChild(createCard(userData.avatar_url, userData.name, userData.login, userData.location, userData.html_url, userData.followers, userData.following))
+})
 
 /* List of LS Instructors Github username's: 
   tetondan
